00422d9782dd73c14977136a1878f4c437e6ef4f
Sega <sega.vasilev@gmail.com>
2022-04-08 09:49:47 +1100

    Update test.py

--- test.py
+++ test.py
@@ -11,15 +11,29 @@ def timevalidator(time):
         return False;
     else :
         return True
+
+
 mask = {
-    "bus_id": {'type': int, 'required': 'required', 'format': '', 'routes':{128: ['Prospect Avenue', 'Elm Street', 'Sunset Bouleward', 'Seasame Street'],
-                                                                           256: ['Seasame Street', 'Fifth Avenue', 'Elm Street', 'Pilotow Street'],
-                                                                           512: ['Sunset Boulevard', 'Bourbon Street']}},
+    "bus_id": {'type': int, 'required': 'required', 'format': ''},
     "stop_id": {'type': int, 'required': 'required', 'format': ''},
     "stop_name": {'type': str, 'required': 'required', 'format': '[Name] [Suffix]'},
     "next_stop": {'type': int, 'required': 'required', 'format': ''},
-    "stop_type": {'type': str, 'required': 'optional', 'format': 'UPPER'},
+    "stop_type": {'type': str, 'required': 'optional', 'format': 'UPPER', 'values': ['S', 'O', 'F', '']},
     "a_time": {'type': str, 'required': 'required', 'format': 'dtime HH:MM'}
+}
+routes = {
+    128: ['Prospect Avenue', 'Elm Street', 'Sunset Bouleward', 'Seasame Street'],
+    256: ['Seasame Street', 'Fifth Avenue', 'Elm Street', 'Pilotow Street'],
+    512: ['Sunset Boulevard', 'Bourbon Street']
+}
+stops = {
+    1: 'Prospect Avenue',
+    2: 'Pilotow Street',
+    3: 'Elm Street',
+    4: 'Bourbon Street',
+    5: 'Fifth Avenue',
+    6: 'Sunset Bouleward',
+    7: 'Seasame Street'
 }
 med = {
     "bus_id": 0,
@@ -41,11 +55,11 @@ for i in range(len(read)):
         med['bus_id'] += 1
     if isinstance(stop_id, mask['stop_id']['type']) == False:
         med['stop_id'] += 1
-    if isinstance(stop_name, mask['stop_name']['type']) == False: #  or len([i for i in stop_name.split(' ') if i.istitle()]) != 2:
+    if isinstance(stop_name, mask['stop_name']['type']) == False or len(stop_name) == 0 or len([i for i in stop_name.split(' ') if i.istitle()]) >= 2 == False:
         med['stop_name'] += 1
     if isinstance(next_stop, mask['next_stop']['type']) == False:
         med['next_stop'] += 1
-    if isinstance(stop_type, mask['stop_type']['type']) == False or (stop_type != '' and stop_type.isupper() == False):
+    if isinstance(stop_type, mask['stop_type']['type']) == False or (stop_type in mask['stop_type']['values']) == False:
         med['stop_type'] += 1
     if isinstance(a_time, mask['a_time']['type']) == False or timevalidator(a_time) == False:
         med['a_time'] += 1
